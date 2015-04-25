f9590770144d9c748e04716442c7b5343e371368
aalawneh <adnan.alawneh@gmail.com>
2015-04-25 17:52:51 -0400

    Update job1Reducer.py

--- job1Reducer.py
+++ job1Reducer.py
@@ -6,6 +6,13 @@
 import sys
 import numpy
 
+# Passing arguments isForW -mapper 'count_mapper.py arg1 arg2'  
+# arg1 = sys.argv[1]
+isForW = True
+
+W = []
+H = []
+
 def read_input(file):
     for line in file:
         # split the line into words
@@ -26,7 +33,7 @@ def main(separator='\t'):
 				VRow = []			
 				vRow = [0]*1682
 				# here we should calculate the gradiant
-				computeGradiant(oldRow)
+				computeGradiant(oldRow, vRow)
 		
 			oldRow = thisRow
 			col, value = value_data.split(',')
@@ -34,14 +41,33 @@ def main(separator='\t'):
 
 	# do not forget to compute gradiant for the last row
 	if thisRow != None:
-		computeGradiant(oldRow)
+		computeGradiant(oldRow, vRow)
 
 	# for testing .. test for the last row like 943
 	if int(oldRow) == 3:
-		print vRow
+		print ""
+		#print vRow
 		#print W[int(oldRow)]
 
-def computeGradiant(movies):
+def computeGradiant(oldRow, vRow):
+	# dH = W'*(W*H-V);
+
+	#print W[int(oldRow)]
+	index = int(oldRow)
+
+	rowW = W[index]
+	WTrans = numpy.matrix.transpose(numpy.array(W))
+	colW = [row[index] for row in WTrans] 
+	colH =[row[index] for row in H]		
+	
+	#numpy.dot(colW, )
+	#print rowW
+	#print "---------------"
+	print vRow	
+	dH = numpy.subtract(numpy.dot(rowW,colH), vRow)
+	#print dH
+
+if __name__ == "__main__":
 	wf = open ( 'w.arr' , 'r')
 	W = [ map(float,line.split(' ')) for line in wf ]
 	#with open('w.arr') as wf:
@@ -50,5 +76,4 @@ def computeGradiant(movies):
 	hf = open ( 'h.arr' , 'r')
 	H = [ map(float,line.split(' ')) for line in hf ]
 
-if __name__ == "__main__":
 	main()
