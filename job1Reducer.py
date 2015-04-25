b1f0d7052ba646a144813994e51dd7710d5665b1
aalawneh <adnan.alawneh@gmail.com>
2015-04-25 19:58:03 -0400

    Update job1Reducer.py

--- job1Reducer.py
+++ job1Reducer.py
@@ -20,47 +20,80 @@ def read_input(file):
 
 def main(separator='\t'):
 	oldRow = None
+	oldColumn = None
 	VRow = []
 	vRow = [0]*1682
+	VColumn = []
+	vColumn = [0]*943
 	line = read_input(sys.stdin)
-	for line in sys.stdin:
+	if isForW == True:
+		for line in sys.stdin:
 
-		data_mapped = line.strip().split('\t')
-		thisRow, value_data = data_mapped
+			data_mapped = line.strip().split('\t')
+			thisColumn, value_data = data_mapped
 
-		if thisRow:		
-			if oldRow and oldRow != thisRow:
-				VRow = []			
-				vRow = [0]*1682
-				# here we should calculate the gradiant
-				computeGradiant(oldRow, vRow)
+			if thisColumn:		
+				if oldColumn and oldColumn != thisColumn:
+					VColumn = []
+					vColumn = [0]*943
+					# here we should calculate the gradiant for user ID x
+					computeGradiant(oldColumn, vColumn)
 		
-			oldRow = thisRow
-			col, value = value_data.split(',')
-			vRow[int(col)] = value
+				oldColumn = thisColumn
+				row, value = value_data.split(',')
+				vColumn[int(row)] = value 
 
-	# do not forget to compute gradiant for the last row
-	if thisRow != None:
-		computeGradiant(oldRow, vRow)
+		# do not forget to compute gradiant for the last row
+		if thisColumn != None:
+			computeGradiant(oldColumn, vColumn)
 
-	# for testing .. test for the last row like 943
-	if int(oldRow) == 3:
-		print ""
-		#print vRow
+		# for testing .. test for the last row like 943
+		if int(oldColumn) == 3:
+			print ""
+			#print vRow
 
-def computeGradiant(oldRow, vRow):
-	# dH = W'*(W*H-V);
+	else:
+		for line in sys.stdin:
 
-	index = int(oldRow)
+			data_mapped = line.strip().split('\t')
+			thisRow, value_data = data_mapped
 
-	rowW = W[index]
-	WTrans = numpy.matrix.transpose(numpy.array(W))
-	colW = [row[index] for row in WTrans] 
-	colH =[row[index] for row in H]		
+			if thisRow:		
+				if oldRow and oldRow != thisRow:
+					VRow = []			
+					vRow = [0]*1682
+					# here we should calculate the gradiant for user ID x
+					computeGradiant(oldRow, vRow)
+		
+				oldRow = thisRow
+				col, value = value_data.split(',')
+				vRow[int(col)] = value 
+
+		# do not forget to compute gradiant for the last row
+		if thisRow != None:
+			computeGradiant(oldRow, vRow)
+
+
+
+
+
+
+def computeGradiant(theIndex, vVector):
+	if isForW == True:
+		# dH = W'*(W*H-V);
+
+		index = int(theIndex)
+
+		WTrans = (numpy.array(W)).transpose()
+
+		colH = H[:,index]
 	
-	#print "---------------"
-	dH = numpy.subtract(numpy.dot(rowW,colH), vRow)
-
+		# dH for column(index)
+		dH = numpy.dot(WTrans, numpy.subtract(numpy.dot(W,colH), vVector))
+		print WTrans
+	else:
+		print ""
+	
 if __name__ == "__main__":
 	wf = open ( 'w.arr' , 'r')
 	W = [ map(float,line.split(' ')) for line in wf ]
