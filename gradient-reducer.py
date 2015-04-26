aa025e71ecd6d1856379e394e310462dd4b715ba
aalawneh <adnan.alawneh@gmail.com>
2015-04-26 14:32:51 -0400

    Create gradient-reducer.py

---
+++ gradient-reducer.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+# Format of each line is:
+#Input: userID::movieID::ratings::timestamp
+
+import sys
+import re
+
+# Passing arguments isForW -mapper 'mapper.py arg1 arg2'
+if sys.argv[1] == "isForW":
+	isForW = True
+
+for line in sys.stdin:
+	if not line.strip(): 
+        	continue
+	data = re.split('\t|\:{2}', line.strip())
+	
+	# send a whole line of V to a reducer	
+	# emit userID,movieID,ratings --> emit(i,j,v)
+	if len(data) >= 3:
+		userID, movieID, ratings, timestamp = data
+		if isForW:
+			key = int(userID) - 1
+			value = str(int(movieID) - 1) + ',' + ratings
+		else:
+			key = int(movieID) - 1
+			value = str(int(userID) - 1) + ',' + ratings
+		print '%s\t%s' % (key, value)
